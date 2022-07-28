8a1851124694e086f556426cf6afef691bc9fe2a
“Marcus <mcolelittle@gmail.com>
2022-07-28 09:07:41 -0400

    add redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,16 @@
 exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
+  const { createPage, createRedirect } = actions
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
   })
+  createRedirect({ 
+    fromPath: `/page-2`, 
+    toPath: `/blog/page-2`, 
+    conditions: { 
+      country: `MX` 
+    } 
+  }) 
 }
